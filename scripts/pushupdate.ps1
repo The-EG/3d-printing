00b6405276351f2600a66965a3260926fca47f82
Taylor Talkington <taylor.talkington@gmail.com>
2022-09-30 07:43:53 -0400

    ✨ Add pushupdates script

---
+++ scripts/pushupdate.ps1
@@ -0,0 +1,23 @@
+Write-Output "Staging profile changes"
+git add 'SuperSlicerProfiles'
+
+if ( !$? ) {
+    Write-Error "Couldn't stage profile files"
+    Exit 1
+}
+
+Write-Output "Creating commit"
+git commit -m "🚀 Automated Profile Update"
+
+if ( !$? ) {
+    Write-Error "Couldn't create commit"
+    Exit 1
+}
+
+Write-Output "Pushing commit"
+git push
+
+if ( !$? ) {
+    Write-Error "Couldn't push commit"
+    Exit 1
+}
